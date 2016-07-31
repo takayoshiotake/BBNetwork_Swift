bbb6260b7c60859befbbad0e1b42657f0fbe426b
OTAKE Takayoshi <takayoshiotake@users.noreply.github.com>
2016-07-31 19:33:10 +0900

    Modify: noun

--- BBNetwork/BBNetwork/HTTPResponse.swift
+++ BBNetwork/BBNetwork/HTTPResponse.swift
@@ -33,25 +33,25 @@ public enum HTTPStatusCode {
 public struct HTTPResponse {
     public let httpVersion: String
     public let statusCode: HTTPStatusCode
-    public let responseHeader: [(String, String)]?
+    public let responseHeaders: [(String, String)]?
     public let body: [UInt8]?
     
     public var responseLine: String {
         return "\(httpVersion) \(statusCode.intValue) \(statusCode.reasonPhrase)"
     }
     
-    public init(httpVersion: String, statusCode: HTTPStatusCode, responseHeader: [(String, String)]?, body: [UInt8]?) {
+    public init(httpVersion: String, statusCode: HTTPStatusCode, responseHeaders: [(String, String)]?, body: [UInt8]?) {
         self.httpVersion = httpVersion
         self.statusCode = statusCode
-        self.responseHeader = responseHeader
+        self.responseHeaders = responseHeaders
         self.body = body
     }
     
     public func bytes() -> [UInt8] {
         var str = responseLine + "\r\n"
-        if let responseHeader = responseHeader {
-            for (header, value) in responseHeader {
-                str += "\(header): \(value)\r\n"
+        if let responseHeaders = responseHeaders {
+            for (key, value) in responseHeaders {
+                str += "\(key): \(value)\r\n"
             }
         }
         str += "\r\n"
